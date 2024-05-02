ceb6998eb86941eb0f964f4d85762c4129f81f3b
Manpreet Singh <76054224+ManpreetS0029@users.noreply.github.com>
2024-05-02 14:37:31 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,51 @@ import { login, logout } from './store/authSlice';
 import { useDispatch } from 'react-redux';
 import Layout from './components/admin/Layout';
 import UserLayout from './components/UserLayout';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // New loading state
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const { pathname } = location;
 
-    pathname == '/admin'
+    pathname === '/admin'
       ? (document.title = 'Tech Blogs - Admin')
       : (document.title = 'Tech Blogs - Get Updated with Latest Tech');
   }, [location]);
 
   useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
+    authService.getCurrentUser().then((userData) => {
+      if (location.pathname === '/admin') {
         if (userData) {
+          navigate('/admin/dashboard');
           dispatch(login({ userData }));
           setIsLoggedIn(true);
         } else {
           dispatch(logout());
           setIsLoggedIn(false);
         }
-      })
-      .finally(() => setLoading(false));
+        setIsLoading(false); // Update loading state when authentication check is complete
+      }
+    });
   }, []);
 
-  return location.pathname == '/admin' ? (
-    !loading ? (
-      isLoggedIn ? (
-        <>
-          <Layout />
-        </>
-      ) : (
-        <Login />
-      )
-    ) : null
-  ) : (
-    <UserLayout />
-  );
+  if (location.pathname === '/admin') {
+    if (isLoading) {
+      return null; // Render nothing while loading
+    }
+
+    if (!isLoggedIn) {
+      return <Login />; // Render Login component if not logged in
+    }
+
+    return <Layout />; // Render Layout component if logged in
+  } else {
+    return <UserLayout />; // Render UserLayout component for non-admin routes
+  }
 }
 
 export default App;
